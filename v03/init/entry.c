765ff2adaa82f5ac9208d7c7f78c26014e94bc38
chinhin <chinhin@263.net>
2022-06-11 15:21:54 +0800

    添加debug方法及相关库函数

---
+++ v03/init/entry.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ * entry.c
+ * Description:  内核的入口函数
+ * =====================================================================================
+ */
+
+#include "console.h"
+#include "debug.h"
+
+int kern_entry()
+{
+	init_debug();
+
+	console_clear();
+
+	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
+
+	panic("test");
+
+	return 0;
+}
+
